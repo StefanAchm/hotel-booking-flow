63c2ddb493d44d94c8d30a8b5330f9352b041d4c
StefanA <stefanachmueller@hotmail.com>
2022-05-09 13:53:30 +0200

    Comp images

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+
 module.exports = {
+  configureWebpack: {
+  plugins: [
+    new ImageminPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 40
+        })
+      ]
+    })
+  ]
+},
   transpileDependencies: [
     'vuetify'
   ]
